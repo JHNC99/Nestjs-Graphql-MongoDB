b43e33be56cb175f75b5ea04b8b1b40553483a22
JHNC99 <jonathanhnc75@gmail.com>
2021-11-25 23:59:26 -0600

    Obtner datos por su id

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -5,15 +5,10 @@ import { LessonService } from './lesson.service';
 @Resolver((of) => LessonType)
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
-  //Get data
+  //Get data for id
   @Query((returns) => LessonType)
-  lesson() {
-    return {
-      id: 'sajdjhashdaj',
-      name: 'Physics Class ',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
   }
   //Create data
   @Mutation((returns) => LessonType)
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -8,6 +8,12 @@ export class LessonService {
   constructor(
     @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
   ) {}
+
+  /* Get lesson for id  */
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({ id });
+  }
+  /*Create lesson service */
   async createLesson(name, startDate, endDate) {
     const lesson = this.lessonRepository.create({
       id: uuid(),
